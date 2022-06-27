10ea793a09f7645f85f60bcdccd3dd96811740e4
Agustín Covarrubias <agucova@uc.cl>
2022-06-27 03:28:49 +0000

    fix: better logout

--- site/borrar_usuarios.php
+++ site/borrar_usuarios.php
@@ -17,6 +17,24 @@ if(!$query) {
 
 // Log out user
 session_start();
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
 session_destroy();
 
 // If no users were imported return to index with a message in the URI
