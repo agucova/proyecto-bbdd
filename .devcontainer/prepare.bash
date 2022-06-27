b1795def47c4a7f91de1f6a93cf5a076654f8207
Agustín Covarrubias <agucova@uc.cl>
2022-06-27 03:23:58 +0000

    fix: add new functions to the devcontainer

--- .devcontainer/prepare.bash
+++ .devcontainer/prepare.bash
@@ -6,8 +6,12 @@ sudo service postgresql start
 psql -d grupo27e3 -f dumps/grupo27.sql
 psql -d grupo44e3 -f dumps/grupo44.sql
 # Load stored functions
+psql -d grupo27e3 -f functions/drop_all_functions.sql
+psql -d grupo27e3 -f functions/generate_reservation.sql
+psql -d grupo27e3 -f functions/generate_ticket.sql
+psql -d grupo27e3 -f functions/has_schedule_conflict.sql
+psql -d grupo27e3 -f functions/is_passport_valid.sql
 psql -d grupo27e3 -f functions/import_users.sql
-psql -d grupo27e3 -f functions/update_proposal.sql
 # Patch broken serial generation of IDs
 psql -d grupo27e3 -f functions/patch_serial.sql
 
